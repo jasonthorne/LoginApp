81153427bddc46170e3c4ebad18d4130d111f132
JasonThorne <jasonthorne@live.ie>
2019-03-17 20:12:20 +0000

    LoginController 1

--- src/sample/controller/DetailsController.java
+++ src/sample/controller/DetailsController.java
@@ -1,4 +1,43 @@
 package sample.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class DetailsController {
+
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label detailsWelcomeLbl;
+
+    @FXML
+    private Label detailsName;
+
+    @FXML
+    private Label detailsAge;
+
+    @FXML
+    private Label detailsProfession;
+
+    @FXML
+    private Label detailsGithub;
+
+    @FXML
+    private Label detailsLinkedIn;
+
+    @FXML
+    void initialize() {
+
+
+
+    }
 }
+
+
--- src/sample/controller/LoginController.java
+++ src/sample/controller/LoginController.java
@@ -5,12 +5,18 @@ package sample.controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 
 public class LoginController {
@@ -45,8 +51,50 @@ public class LoginController {
         loginBtn.setOnAction(new EventHandler<ActionEvent>() { //give button an event handler as its action
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("button clicked!");
+
+                System.out.println("login button clicked!");
+
+                //invoke loginUser:
+                loginUser();
             }
         });
+    }
+
+    private void loginUser(){
+
+        //+++++++++++++IMPORTANT!!!
+        //get the login window by targeting an element of it (loginBtn), and disable the window's visibility;
+        loginBtn.getScene().getWindow().hide(); //Use the button to get the scene where the button resides, and use that to get the window, then call '.hide' on the window. (crazy!)
+
+        if (!loginUserName.getText().toString().trim().equals("") //if username text field is NOT empty
+                && !loginPassword.getText().toString().trim().equals(""));//AND password text field is NOT empty
+        {
+
+            //create a new stage:
+            Stage detailsStage = new Stage();
+
+            //create a new root from FXML file. Try Catch needed as its file reading, so possibly a fallover point:
+            try{
+                Parent root = FXMLLoader.load(getClass().getResource("/sample/view/details.fxml"));
+
+                //create a new scene, and pass it the root:
+                Scene scene = new Scene(root);
+
+                //add the scene to the stage:
+                detailsStage.setScene(scene);
+
+                //show the stage!! ++++
+                detailsStage.show();
+
+                //disallow resizing of window by user
+                detailsStage.setResizable(false);
+
+            }catch (IOException e){
+
+                e.printStackTrace();
+            }
+
+        }
+
     }
 }
